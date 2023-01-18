99c82ba73febb5cf4051443473853f6f3596c506
Matheus <matheusvclls97@gmail.com>
2023-01-18 18:02:02 -0300

    feat: add models (table set up) to the Activity

---
+++ jobs/models/activity_models.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from sqlalchemy import Column, Integer, String, Text, Float
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'db'))
+from db_setup import Base
+
+
+class Activity(Base):
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(200), nullable=False)
+    distance = Column(Float, nullable=True)
+    average_speed = Column(Float, nullable=True)
+    average_heartrate = Column(Float, nullable=True)
