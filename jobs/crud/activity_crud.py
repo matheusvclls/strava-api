0e32d169c2ab6c430885af330b9d1b5c4890c5ff
Matheus <matheusvclls97@gmail.com>
2023-01-18 18:10:55 -0300

    feat: add functions to get and create a new row

---
+++ jobs/crud/activity_crud.py
@@ -0,0 +1,31 @@
+from sqlalchemy.orm import Session
+import sys
+import os 
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'models'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'schemas'))
+
+from activity_schemas import ActivityCreate
+from activity_models import Activity
+
+
+def get_strava_activity(db: Session, activity_id: int):
+    result = db.query(Activity).filter(Activity.id == activity_id).first()
+    return result
+
+
+def create_activity(db: Session, activity: ActivityCreate):
+    db_activity = Activity(
+        id=activity['id'],
+        name=activity['name'],
+        distance=activity['distance'],
+        average_speed=activity['average_speed'],
+        average_heartrate=activity['average_heartrate']
+    )
+    db.add(db_activity)
+    db.commit()
+    db.refresh(db_activity)
+    return db_activity
+
+
