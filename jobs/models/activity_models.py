5c2812d3e5f74565aed0b55d40acc2fa53dd5858
Matheus <matheusvclls97@gmail.com>
2023-01-18 19:48:29 -0300

    fix: change Integer type to BigInteger

--- jobs/models/activity_models.py
+++ jobs/models/activity_models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, Integer, String, Text, Float
+from sqlalchemy import Column, Integer, String, Text, Float,BigInteger
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'db'))
 from db_setup import Base
@@ -9,7 +9,7 @@ from db_setup import Base
 class Activity(Base):
     __tablename__ = "activities"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(BigInteger, primary_key=True, index=True)
     name = Column(String(200), nullable=False)
     distance = Column(Float, nullable=True)
     average_speed = Column(Float, nullable=True)
