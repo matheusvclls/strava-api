f0a67fa5f4bf3a598b9a2424e41a01b770251c14
Matheus <matheusvclls97@gmail.com>
2023-02-06 22:48:04 -0300

    feat: add message if you ran this week or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,44 +4,75 @@ import './App.css';
 
 function App() {
 
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-  
-    useEffect(() => {
-      async function fetchData() {
-        setIsLoading(true);
-        try {
-          const response = await axios.get("http://localhost:8000/activities?skip=0&limit=5", { timeout: 5000 });
-          setData(response.data);
-        } catch (error) {
-          if(error.code === 'ECONNABORTED'){
-            setError('The request took too long to complete, please try again later.')
-          }
-          else{
-            setError(error);
-          }
-        } finally {
-          setIsLoading(false);
-        }        
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const response = await axios.get("http://localhost:8000/activities?skip=0&limit=5", { timeout: 5000 });
+        setData(response.data);
+      } catch (error) {
+        if (error.code === 'ECONNABORTED') {
+          setError('The request took too long to complete, please try again later.')
+        }
+        else {
+          setError(error);
+        }
+      } finally {
+        setIsLoading(false);
       }
-      fetchData();
-    }, []);
-  
-    if (isLoading) {
-      return <p>Loading...</p>;
     }
-  
-    if (error) {
-      return <p>Error: {error.message}</p>;
+    fetchData();
+
+    async function getKmsRunning() {
+      setIsLoading(true);
+      try {
+        const response = await axios.get("http://localhost:8000/metrics/total_kms_running_this_week", { timeout: 5000 });
+
+        if (response.data > 0) {
+          setMessage("You ran " + response.data.toFixed(2) + " in this week")
+
+        }
+        else {
+
+          setMessage("You did not run in this week")
+        }
+        ;
+      } catch (error) {
+        if (error.code === 'ECONNABORTED') {
+          setError('The request took too long to complete, please try again later.')
+        }
+        else {
+          setError(error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
-  
-    if (!data) {
-      return null;
-    }
-  
-    return (<div className="App">
-      <p>Last 5 activities</p>
+    getKmsRunning();
+
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  return (<div className="App">
+    <p>Weekly Report</p>
+    <p>{message}</p>
+    <p>Last 5 activities</p>
     <table>
       <thead>
         <tr>
@@ -64,8 +95,8 @@ function App() {
         ))}
       </tbody>
     </table>
-    </div>
-    );
-  }
-    
+  </div>
+  );
+}
+
 export default App
